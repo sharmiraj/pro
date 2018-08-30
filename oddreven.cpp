152f30850309171bc89d037bc5c8d77d94cad6ab
sharmiraj <sharmiraj128@gmail.com>
2018-08-30 21:58:32 +0530

    m

---
+++ oddreven.cpp
@@ -0,0 +1,18 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+cout<<"enter the number:";
+cin>>n;
+if(n%2==0)
+{
+cout<<"the given number is even";
+}
+else
+{
+cout<<"the number is odd";
+}
+getch()
+}
